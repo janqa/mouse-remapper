6e6880e87f21aba694f8529d5b5c6498188125b7
boris <barysdzemidziuk@gmail.com>
2021-04-15 20:19:19 +0300

    created the acme profile

---
+++ acme-profile.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import asyncio
+import evdev
+import evdev.ecodes as ecodes
+
+"""
+This mapping is for the Acme editor. It just makes both side buttons
+(BTN_SIDE and BTN_EXTRA) behave like the wheel button, so it's
+easier to press, drag, and chord.
+"""
+
+# Listen to events from the mouse
+# TODO Implement mouse finder (right now upon every boot
+# I have to search for it manually)
+mouse = evdev.InputDevice("/dev/input/event9")   # /dev/input/event?
+
+# Create virtual mouse
+virtual_mouse = evdev.UInput.from_device(mouse, name="virtual-mouse")
+
+# Lock the real mouse so noone hears it
+mouse.grab()
+
+# Read the mouse loop and write events to the virtual mouse conditionally
+async def handle_mouse():
+    async for event in mouse.async_read_loop():
+        if event.type == ecodes.EV_KEY:
+            key_event = evdev.categorize(event)
+            print(key_event.keycode)
+            if key_event.keycode == "BTN_SIDE" or key_event.keycode == "BTN_EXTRA":
+                state = key_event.keystate
+                virtual_mouse.write(ecodes.EV_KEY, ecodes.BTN_MIDDLE, state)
+                continue
+            else:
+                virtual_mouse.write_event(event)
+        else:
+            virtual_mouse.write_event(event)
+                                                                                        
+asyncio.ensure_future(handle_mouse())
+
+event_loop = asyncio.get_event_loop()
+event_loop.run_forever()
+
